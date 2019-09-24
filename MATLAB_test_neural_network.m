9bb6cf768696ed96f6f31a01b5ecf67cbe78b294
nboland-code <55711700+nboland-code@users.noreply.github.com>
2019-09-24 13:31:25 -0400

    files from testing that worked

---
+++ MATLAB_test_neural_network.m
@@ -0,0 +1,57 @@
+%---- this actually works ----%
+% Please note: these are 4 of the 10 categories available
+% Feel free to choose which ever you like best!
+categories = {'Deer','Dog','Frog','Cat'};
+rootFolder = 'cifar10Train';
+imds = imageDatastore(fullfile(rootFolder, categories), ...
+    'LabelSource', 'foldernames');
+
+% The following architecture is defining layers
+varSize = 32;
+conv1 = convolution2dLayer(5,varSize,'Padding',2,'BiasLearnRateFactor',2);
+conv1.Weights = gpuArray(single(randn([5 5 3 varSize])*0.0001));
+fc1 = fullyConnectedLayer(64,'BiasLearnRateFactor',2);
+fc1.Weights = gpuArray(single(randn([64 576])*0.1));
+fc2 = fullyConnectedLayer(4,'BiasLearnRateFactor',2);
+fc2.Weights = gpuArray(single(randn([4 64])*0.1));
+layers = [
+    imageInputLayer([varSize varSize 3]);
+    conv1;
+    maxPooling2dLayer(3,'Stride',2);
+    reluLayer();
+    convolution2dLayer(5,32,'Padding',2,'BiasLearnRateFactor',2);
+    reluLayer();
+    averagePooling2dLayer(3,'Stride',2);
+    convolution2dLayer(5,64,'Padding',2,'BiasLearnRateFactor',2);
+    reluLayer();
+    averagePooling2dLayer(3,'Stride',2);
+    fc1;
+    reluLayer();
+    fc2;
+    softmaxLayer()
+    classificationLayer()];
+
+% Defining training options
+% Change these to see how your results and accuracy change
+opts = trainingOptions('sgdm', ...
+    'InitialLearnRate', 0.001, ...
+    'LearnRateSchedule', 'piecewise', ...
+    'LearnRateDropFactor', 0.1, ...
+    'LearnRateDropPeriod', 8, ...
+    'L2Regularization', 0.004, ...
+    'MaxEpochs', 100, ...
+    'MiniBatchSize', 100, ...
+    'Verbose', true);
+
+% One line code to train the network
+[net, info] = trainNetwork(imds, layers, opts);
+
+% Load test data
+rootFolder = 'cifar10Test';
+imds_test = imageDatastore(fullfile(rootFolder, categories), ...
+    'LabelSource', 'foldernames');
+
+% This could take a while if you are not using a GPU
+confMat = confusionmat(imds_test.Labels, labels);
+confMat = confMat./sum(confMat,2);
+mean(diag(confMat))
